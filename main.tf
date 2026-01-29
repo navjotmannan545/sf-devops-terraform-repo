9a981ce91baec371fb147540e950026582654eb4
navjotmannan545 <navjot.mannan@tredence.com>
2026-01-29 09:54:58 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
+    }
+  }
+
+  backend "remote" {
+    organization = "sf-terraform-demo-nav"
+
+    workspaces {
+      name = "nav-sf-tf-actions-demo"
+    }
+  }
+}
+
+provider "snowflake" {
+}
+
+resource "snowflake_database" "demo_db" {
+  name    = "DEMO_DB"
+  comment = "Database for Snowflake Terraform demo"
+}
